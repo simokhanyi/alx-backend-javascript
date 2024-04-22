a2bfba4f0ef0f9d9b391e85c1f0f4fe1c6a0af30
Simo Khanyi <simokhanyi@gmail.com>
2024-04-22 22:10:02 +0000

    ES6_Basics

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,14 +1,14 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
-export default function getFullBudgetObject(incomeAmount, gdpAmount, capitaAmount) {
-  const budget = getBudgetObject(incomeAmount, gdpAmount, capitaAmount);
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `$${incomeAmount}`;
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros() {
-      return `${incomeAmount} euros`;
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
 
