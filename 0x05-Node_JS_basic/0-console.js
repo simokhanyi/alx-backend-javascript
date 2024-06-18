a20e0a8a09ae09ca36fc9b471e73d9c1ee1515cd
Simo Khanyi <simokhanyi@gmail.com>
2024-06-18 23:53:06 +0000

    Basic JavaScript

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,7 @@
+/**
+ * Displays the provided message to the console.
+ * @param {string} message - The message to be displayed.
+ */
 function displayMessage(message) {
     console.log(message);
 }
